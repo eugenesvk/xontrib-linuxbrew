c771e3ef0e3405435042dce675947ecbcbda526b
eugenesvk <eugenesvk@users.noreply.github.com>
2021-07-24 22:47:52 +0700

    update classifiers with Python 3.9

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
     "Topic :: System :: Shells",
